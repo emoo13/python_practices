765275411ad294d5d70bb997bd801faca15dfa70
Nate Puckett <codenate05@outlook.com>
2024-05-14 14:44:21 -0600

    Strings homework

---
+++ practice_projects/#Homework_pt.py
@@ -0,0 +1,34 @@
+#Homework pt.1
+
+Friend= "\nSoftware is my friend!\n"
+print (Friend)
+
+#Homework pt.2
+
+Soft = "Software is"
+my = "my friend\n"
+print (Soft,my)
+
+#Homework pt.3
+
+variable_1 = "I would like this to be a string\n"
+print (variable_1)
+
+#Homework pt.4
+
+long_string1 = "This is a super long string.\n"
+long_string2 = "        When I print this to the window, it will continue on the same line\n"
+long_string3 = "        unless we do something about it.\n\n"
+long_string4 = "This is for the homework, I would like it to look facy, please!\n"
+complete_string = long_string1 + long_string2 + long_string3 + long_string4
+print (complete_string)
+
+#Homework pt.5
+
+up = "HeRe iS a harD to ReAd SenTenCe In RandOM CapItAls\n"
+print(up.upper())
+
+#Homework pt.6
+
+low = "HeRe iS a harD to ReAd SenTenCe In RandOM CapItAls\n"
+print(low.lower())
